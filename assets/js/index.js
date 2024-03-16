f6bcc24c4987b43340e3f25e924a0958c602c484
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-16 09:36:29 -0300

    making animations update in my website

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,17 @@
+const watching = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("showing");
+        } else {
+            entry.target.classList.remove("showing");
+        }
+    });
+});
+
+const hiddenClass = document.querySelectorAll(".hidden");
+hiddenClass.forEach((e) => watching.observe(e));
+
 function developing() {
-    alert('Ainda está em desenvolvimento')
+    alert("Ainda está em desenvolvimento");
 }
