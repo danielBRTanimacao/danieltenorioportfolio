455ef85899a55499c5dd01773c3818179c1e2309
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-16 11:13:17 -0300

    new features

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,5 @@
 const watching = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry);
         if (entry.isIntersecting) {
             entry.target.classList.add("showing");
         } else {
