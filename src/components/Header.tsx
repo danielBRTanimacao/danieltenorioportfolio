4e33279eeedc6ddd351c975f5eb9ca7c3ea93ac5
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-09-28 15:27:36 -0300

    collecting colors and creating feture to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,26 @@
 export default () => {
     return (
         <>
-            <header>
-                <h2>nav</h2>
-                <nav>
-                    <ul>
-                        <li>navegação</li>
+            <header className="navbar navbar-expand-lg">
+                <h2 className="navbar-brand">Title</h2>
+                <nav className="navbar-collapse">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <span>1.</span>
+                            <a href="#">HEader</a>
+                        </li>
+                        <li className="nav-item">
+                            <span>2.</span>
+                            <a href="#">Projects</a>
+                        </li>
+                        <li className="nav-item">
+                            <span>3.</span>
+                            <a href="#">sobre mim</a>
+                        </li>
+                        <li className="nav-item">
+                            <span>4.</span>
+                            <a href="#">tecnologia</a>
+                        </li>
                     </ul>
                 </nav>
             </header>
