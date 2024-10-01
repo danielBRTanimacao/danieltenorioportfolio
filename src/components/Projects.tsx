7770a06fccff88044ea45a62f2f5671598b6f4f9
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-10-01 10:58:58 -0300

    creating simple feature to project tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,3 +1,5 @@
+import ColProject from "./ColProject";
+
 import Climar from "../assets/svgs/climar.svg";
 import EbookWorld from "../assets/svgs/ebook-response.svg";
 
@@ -6,8 +8,62 @@ import GProject from "../assets/imgs/projects/PROJETOS GITHUB.png";
 import PSchool from "../assets/imgs/projects/PROJETOS SCHOOL.png";
 import PUrubu from "../assets/imgs/projects/PROJETO URUBU.png";
 import DRest from "../assets/imgs/projects/DESAFIO REST.png";
+import QrProject from "../assets/imgs/projects/QR CODE PROJECT.png";
 
 export default () => {
+    const listImg: string[] = [
+        DRest,
+        QrProject,
+        PUrubu,
+        DPicPay,
+        PSchool,
+        GProject
+    ];
+    const titleList: React.ReactNode[] = [
+        <>
+            Desafio <span style={{ color: "var(--gr-code)" }}>Django</span>{" "}
+            RestApi
+        </>,
+        <>
+            {" "}
+            <span style={{ color: "var(--code-pink)" }}>QrCode</span> Gerente
+            Ar-condicionado
+        </>,
+        <>
+            Urubu do pix{" "}
+            <span style={{ color: "var(--code-purple)" }}>PHP</span>
+        </>,
+        <>
+            Desafio{" "}
+            <span style={{ color: "var(--gr-code)" }}>PicPay Django </span>
+            RestAPI
+        </>,
+        <>I Projeto faculdade</>,
+        <>Estudos gerais</>
+    ];
+    const textList: string[] = [
+        `
+        Desafio para uma empresa utilizando framework
+        django + django rest. o desafio consiste em uma
+        API para gerenciar livros e autores, incluindo
+        autenticação de usuários e funcionalidade de
+        pesquisa...
+        `,
+        "Gerenciamento de ar condicionados de uma empresa local, o site gera um QrCode para os ar-condicionado dos clientes onde é possível escanear ver informações sobre aquele ar especifico pode ser escaneado por qualquer pessoa, mas só e possível ser editado por gerentes específicos feito com django + boostrap",
+        "Projeto urubu do pix feito com o simples html, css, javascript e para backend php...",
+        "PICPAY SIMPLIFICADO com Python e Django Desafio",
+        "O projeto de extensão tem como principal objetivo melhorar a comunidade em que vivemos... Isso significa que, ao desenvolver um projeto de extensão, estamos direcionando nossas habilidades em Análise e Desenvolvimento de Sistemas para criar soluções que beneficiem as pessoas ao nosso redor...um exemplo prático",
+        "Estudos gerais repositorio GitHub"
+    ];
+    const listLinkGit: string[] = [
+        "https://github.com/danielBRTanimacao/desafio-spotter-django-rest",
+        "https://github.com/danielBRTanimacao/qr-code-maker-python",
+        "https://github.com/danielBRTanimacao/urubu-do-pix-php",
+        "https://github.com/danielBRTanimacao/desafio-picpay-django",
+        "https://github.com/danielBRTanimacao/I-projeto-extensao-faculdade",
+        "https://github.com/danielBRTanimacao?tab=repositories"
+    ];
+
     return (
         <>
             <section className="px-4" id="projects">
@@ -78,7 +134,7 @@ export default () => {
             </section>
 
             <section className="px-4">
-                <h1 className="display-4">
+                <h1 className="display-4 py-5">
                     <span style={{ color: "var(--gr-code)" }}>
                         Outros Projetos
                     </span>
@@ -86,123 +142,15 @@ export default () => {
                     <span style={{ color: "var(--code-orange)" }}>{"{}"}</span>
                 </h1>
                 <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                    <div className="col">
-                        <div className="card">
-                            <h5 className="text-center">
-                                Desafio{" "}
-                                <span style={{ color: "var(--gr-code)" }}>
-                                    Django
-                                </span>{" "}
-                                RestApi
-                            </h5>
-                            <img
-                                src={DRest}
-                                className="img-fluid"
-                                alt="img-project"
-                            />
-                        </div>
-                        <div className="card-body">
-                            <p className="card-text">
-                                Desafio para uma empresa utilizando framework
-                                django + django rest. o desafio consiste em uma
-                                API para gerenciar livros e autores, incluindo
-                                autenticação de usuários e funcionalidade de
-                                pesquisa...
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card">
-                            <h5 className="text-center">
-                                <span style={{ color: "var(--code-pink)" }}>
-                                    QrCode
-                                </span>{" "}
-                                Gerente Ar-condicionado
-                            </h5>
-                            <img
-                                src="https://fakeimg.pl/470x200/"
-                                className="img-fluid"
-                                alt="img-project"
-                            />
-                        </div>
-                        <div className="card-body">
-                            <p className="card-text">
-                                Lorem ipsum dolor sit amet.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card">
-                            <h5 className="text-center">
-                                Urubu do pix{" "}
-                                <span style={{ color: "var(--code-purple)" }}>
-                                    PHP
-                                </span>
-                            </h5>
-                            <img
-                                src={PUrubu}
-                                className="img-fluid"
-                                alt="img-project"
-                            />
-                        </div>
-                        <div className="card-body">
-                            <p className="card-text">
-                                projeto urubu do pix feito com o simples html,
-                                css, javascript e para backend php...
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card">
-                            <h5 className="text-center">
-                                Desafio{" "}
-                                <span style={{ color: "var(--gr-code)" }}>
-                                    PicPay Django{" "}
-                                </span>
-                                RestAPI
-                            </h5>
-                            <img
-                                src={DPicPay}
-                                className="img-fluid"
-                                alt="img-project"
-                            />
-                        </div>
-                        <div className="card-body">
-                            <p className="card-text">
-                                Lorem ipsum dolor sit amet.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card">
-                            <h5 className="text-center">I Projeto faculdade</h5>
-                            <img
-                                src={PSchool}
-                                className="img-fluid"
-                                alt="img-project"
-                            />
-                        </div>
-                        <div className="card-body">
-                            <p className="card-text">
-                                Lorem ipsum dolor sit amet.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card">
-                            <h5 className="text-center">Estudos gerais</h5>
-                            <img
-                                src={GProject}
-                                className="img-fluid"
-                                alt="img-project"
-                            />
-                        </div>
-                        <div className="card-body">
-                            <p className="card-text">
-                                Lorem ipsum dolor sit amet.
-                            </p>
-                        </div>
-                    </div>
+                    {listImg.map((img, index) => (
+                        <ColProject
+                            key={index}
+                            img={img}
+                            title={titleList[index]}
+                            text={textList[index]}
+                            linkGit={listLinkGit[index]}
+                        />
+                    ))}
                 </div>
             </section>
         </>
