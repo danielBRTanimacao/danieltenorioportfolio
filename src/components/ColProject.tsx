581eff2e6c22cfe0f992edaeb162ae0a9ab70729
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-07-02 19:21:39 -0300

    align content

--- src/components/ColProject.tsx
+++ src/components/ColProject.tsx
@@ -2,7 +2,7 @@ export default ({
     img,
     title,
     text,
-    linkGit
+    linkGit,
 }: {
     img: string;
     title: React.ReactNode;
@@ -10,23 +10,27 @@ export default ({
     linkGit: string;
 }) => {
     return (
-        <>
-            <div className="col">
-                <div className="card">
-                    <h5 className="text-center">{title}</h5>
-                    <img src={img} className="img-fluid" alt="img-project" />
-                </div>
+        <div className="col d-flex justify-content-center">
+            <div className="card text-truncate">
+                <h5 className="text-center">{title}</h5>
+                <div
+                    className="img-project"
+                    style={{
+                        backgroundImage: `url(${img})`,
+                    }}
+                ></div>
                 <div className="card-body">
-                    <p className="card-text text-truncate">{text}</p>
+                    <p className="card-text">{text}</p>
                     <a
                         href={linkGit}
                         target="_blank"
                         className="btn btn-outline-light btn-sm"
+                        rel="noopener noreferrer"
                     >
                         Saiba mais
                     </a>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
